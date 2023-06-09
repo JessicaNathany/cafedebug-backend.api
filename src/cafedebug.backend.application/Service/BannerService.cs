27ebdea10604c515ffe13841e1161e75bf24d934
Jéssica <jessicanathany.web@gmail.com>
2023-06-09 19:19:32 -0300

    ajustes na arquitetura

---
+++ src/cafedebug.backend.application/Service/BannerService.cs
@@ -0,0 +1,28 @@
+﻿using cafedebug_backend.domain.Entities;
+using cafedebug_backend.domain.Interfaces.Services;
+
+namespace cafedebug.backend.application.Service
+{
+    internal class BannerService : IBannerService
+    {
+        public Task Delete(Guid code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save(Banner banner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Banner banner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
