a989ff518bdce17e26df6a305acd050533d41eb7
Jéssica <jessicanathany.web@gmail.com>
2024-07-08 19:50:25 -0300

    criação das classes AuthControllerTest e JWTServiceTest

---
+++ tests/cafedebug.backend.api.test/Controllers/AuthControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cafedebug.backend.api.test.Controllers
+{
+    public class AuthControllerTest
+    {
+    }
+}
