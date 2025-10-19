60ec9bfa15b0374a9fc622f5816b7402008a7f56
Regis <regisbarros21@gmail.com>
2025-10-19 19:48:32 -0300

    Remove EmailValidator and UserModel classes as part of cleanup and restructuring

--- src/cafedebug-backend.api/Models/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace cafedebug_backend.api.ViewModels
-{
-    public class UserModel
-    {
-        public string Password { get; set; }
-
-        public string Email { get; set; }
-    }
-}
