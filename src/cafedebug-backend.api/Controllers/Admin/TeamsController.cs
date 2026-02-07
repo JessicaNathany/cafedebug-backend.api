e62de0bf1103157121af7afffc86a5663f108d6a
Regis <regisbarros21@gmail.com>
2026-02-07 19:08:50 -0300

    Rename TeamsController.cs to TeamMemberController.cs

--- src/cafedebug-backend.api/Controllers/Admin/TeamsController.cs
+++ src/cafedebug-backend.api/Controllers/Admin/TeamsController.cs
@@ -6,63 +6,75 @@ using cafedebug.backend.application.Podcasts.DTOs.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace cafedebug_backend.api.Controllers.Admin
+namespace cafedebug_backend.api.Controllers.Admin;
+
+[ApiController]
+[Produces("application/json")]
+[Route("api/v1/admin/team-members")]
+[Tags("Admin - Team Members")]
+public class TeamsController(ITeamMemberService teamMemberService) : ControllerBase
 {
-    [ApiController]
-    [Produces("application/json")]
-    [Route("api/v1/admin/teams")]
-    [Tags("Admin - Teams")]
-    public class TeamsController(ITeamMemberService teamMemberService) : ControllerBase
+    [HttpPost]
+    [Authorize]
+    [ProducesResponseType(typeof(TeamMemberResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<Result>> CreateAsync([FromBody] TeamMemberRequest request)
     {
-        [HttpPost]
-        [Authorize]
-        [ProducesResponseType(typeof(TeamMemberResponse), StatusCodes.Status201Created)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<Result>> CreateAsync([FromBody] TeamMemberRequest request)
-        {
-            return await teamMemberService.CreateAsync(request);
-        }
+        return await teamMemberService.CreateAsync(request);
+    }
 
-        [HttpPut]
-        [Route("{id:int}")]
-        [Authorize]
-        [ProducesResponseType(typeof(TeamMemberResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<Result>> UpdateAsync(int id, [FromBody] TeamMemberRequest request)
-        {
-            return await teamMemberService.UpdateAsync(id, request);
-        }
+    [HttpPut]
+    [Route("{id:int}")]
+    [Authorize]
+    [ProducesResponseType(typeof(TeamMemberResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<Result>> UpdateAsync(int id, [FromBody] TeamMemberRequest request)
+    {
+        return await teamMemberService.UpdateAsync(id, request);
+    }
 
-        [HttpGet]
-        [ProducesResponseType(typeof(PagedResult<TeamMemberResponse>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<Result>> GetAllAsync([FromQuery] PageRequest request)
-        {
-            return await teamMemberService.GetAllAsync(request);
-        }
+    [HttpGet]
+    [Authorize]
+    [ProducesResponseType(typeof(PagedResult<TeamMemberResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<Result>> GetAllAsync([FromQuery] PageRequest request)
+    {
+        return await teamMemberService.GetAllAsync(request);
+    }
 
-        [HttpGet]
-        [Route("{id:int}")]
-        [ProducesResponseType(typeof(TeamMemberResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<Result>> GetByIdAsync(int id)
-        {
-            return await teamMemberService.GetByIdAsync(id);
-        }
+    [HttpGet]
+    [Route("{id:int}")]
+    [Authorize]
+    [ProducesResponseType(typeof(TeamMemberResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<Result>> GetByIdAsync(int id)
+    {
+        return await teamMemberService.GetByIdAsync(id);
+    }
         
 
-        [HttpDelete]
-        [Route("{id:int}")]
-        [Authorize]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<Result>> Delete(int id)
-        {
-            return await teamMemberService.DeleteAsync(id);
-        }
+    [HttpDelete]
+    [Route("{id:int}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<Result>> Delete(int id)
+    {
+        return await teamMemberService.DeleteAsync(id);
     }
 }
