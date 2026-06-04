e630cdf80c33c6b5673cf638ffdc390dc5e6adb6
Jéssica <jessicanathany.web@gmail.com>
2026-06-04 20:55:25 -0300

    created BannerStatus enum and added status in Banner

---
+++ src/cafedebug-backend.domain/Banners/BannerStatus.cs
@@ -0,0 +1,20 @@
+﻿namespace cafedebug_backend.domain.Banners;
+public sealed record BannerStatus(string Value)
+{
+    public static readonly BannerStatus Draft = new("draft");
+    public static readonly BannerStatus Published = new("published");
+    public static readonly BannerStatus Archived = new("archived");
+    public static readonly BannerStatus Scheduled = new("scheduled");
+    public override string ToString() => Value;
+
+    public static implicit operator string(BannerStatus status) => status.Value;
+
+    public static implicit operator BannerStatus(string status) => status switch
+    {
+        "draft" => Draft,
+        "published" => Published,
+        "archived" => Archived,
+        "scheduled" => Scheduled,
+        _ => throw new ArgumentException("Invalid banner status", nameof(status))
+    };
+}
